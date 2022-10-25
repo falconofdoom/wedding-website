8065ceb9de5187fdc52e04519f51b8d1fef8771f
FalconOfDoom <alcaideredb@gmail.com>
2022-10-25 18:23:40 +0800

    Add rsvp form

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,48 @@
-'use strict';
+"use strict";
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
+var mergeStream = require('merge-stream');
 
 // compile scss to css
-gulp.task('sass', function () {
-    return gulp.src('./sass/styles.scss')
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({basename: 'styles.min'}))
-        .pipe(gulp.dest('./css'));
+gulp.task("sass", function () {
+  var mainStyles = gulp
+    .src("./sass/styles.scss")
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(rename({ basename: "styles.min" }))
+    .pipe(gulp.dest("./css"));
+  var rsvpFormStyles = gulp
+    .src("./sass/rsvp-form.scss")
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(rename({ basename: "rsvp-form.min" }))
+    .pipe(gulp.dest("./css"));
+
+  return mergeStream(mainStyles, rsvpFormStyles);
 });
 
 // watch changes in scss files and run sass task
-gulp.task('sass:watch', function () {
-    gulp.watch('./sass/**/*.scss', ['sass']);
+gulp.task("sass:watch", function () {
+  gulp.watch("./sass/**/*.scss", ["sass"]);
 });
 
 // minify js
-gulp.task('minify-js', function () {
-    return gulp.src('./js/scripts.js')
-        .pipe(uglify())
-        .pipe(rename({basename: 'scripts.min'}))
-        .pipe(gulp.dest('./js'));
+gulp.task("minify-js", function () {
+    var scriptsJS = gulp
+    .src("./js/scripts.js")
+    .pipe(uglify())
+    .pipe(rename({ basename: "scripts.min" }))
+    .pipe(gulp.dest("./js"));
+
+    var rsvpFormJS = gulp
+    .src("./js/rsvp-form.js")
+    .pipe(uglify())
+    .pipe(rename({ basename: "rsvp-form.min" }))
+    .pipe(gulp.dest("./js"));
+  
+    return mergeStream(scriptsJS, rsvpFormJS);
 });
 
 // default task
-gulp.task('default', gulp.series('sass', 'minify-js'));
+gulp.task("default", gulp.series("sass", "minify-js"));
