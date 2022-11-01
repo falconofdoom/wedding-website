24f146c7fcc171e0b88121e5176b75f118d26c37
Redan Benedict Alcaide <alcaideredb@gmail.com>
2022-11-02 04:17:38 +0800

    Revert "Enable prod backend (#2)" (#3)
    This reverts commit 881e2d7a4ae5522dd564b5c1d73fb465a2f9ff07.

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,30 @@
-"use strict";
+'use strict';
 
-var gulp = require("gulp");
-var sass = require('gulp-sass')(require('sass'));
-
-const terser = require('gulp-plugin-terser');
-var rename = require("gulp-rename");
-var mergeStream = require('merge-stream');
-
-const options = {
-  suffix: '.min.js',
-  terserOptions: {
-    output: {
-      comments: false
-    }
-  }
-};
-
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 // compile scss to css
-gulp.task("sass", function () {
-  var mainStyles = gulp
-    .src("./sass/styles.scss")
-    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-    .pipe(rename({ basename: "styles.min" }))
-    .pipe(gulp.dest("./css"));
-  var rsvpFormStyles = gulp
-    .src("./sass/rsvp-form.scss")
-    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-    .pipe(rename({ basename: "rsvp-form.min" }))
-    .pipe(gulp.dest("./css"));
-
-  return mergeStream(mainStyles, rsvpFormStyles);
+gulp.task('sass', function () {
+    return gulp.src('./sass/styles.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({basename: 'styles.min'}))
+        .pipe(gulp.dest('./css'));
 });
 
 // watch changes in scss files and run sass task
-gulp.task("sass:watch", function () {
-  gulp.watch("./sass/**/*.scss", ["sass"]);
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
 // minify js
-gulp.task("minify-js", function () {
-    var scriptsJS = gulp
-    .src("./js/scripts.js")
-    .pipe(terser(options))
-    .pipe(rename({ basename: "scripts.min" }))
-    .pipe(gulp.dest("./js"));
-
-    var rsvpFormJS = gulp
-    .src("./js/rsvp-form.js")
-    .pipe(terser(options))
-    .pipe(rename({ basename: "rsvp-form.min" }))
-    .pipe(gulp.dest("./js"));
-  
-    return mergeStream(scriptsJS, rsvpFormJS);
+gulp.task('minify-js', function () {
+    return gulp.src('./js/scripts.js')
+        .pipe(uglify())
+        .pipe(rename({basename: 'scripts.min'}))
+        .pipe(gulp.dest('./js'));
 });
 
 // default task
-gulp.task("default", gulp.series("sass", "minify-js"));
+gulp.task('default', gulp.series('sass', 'minify-js'));
